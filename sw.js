31030296ba439b0fbb504189407285093d526008
liammurray123 <liam.murray123@icloud.com>
2021-01-18 09:33:27 -0800

    added cache paths

--- sw.js
+++ sw.js
@@ -3,9 +3,12 @@ const assets = [
   '/public/css/style.css',
   '/public/fonts/Rawkbrush.otf',
   '/public/js/animations.js',
-  '/public/pwaicons/',
+  '/public/js/firestore.js',
+  '/public/pwaicons/apple-icon-180.png',
+  '/public/pwaicons/manifest-icon-192.png',
+'/public/pwaicons/manifest-icon-512.png',
   '/public/views/index.html',
-  './'
+  '/public/views/404.html'
 ];
 
 self.addEventListener('install', evt => {
