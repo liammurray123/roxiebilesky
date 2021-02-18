20193dfd936485b046765483bc03a3e5c3baf7a4
liammurray123 <liam.murray123@icloud.com>
2021-02-18 15:27:24 -0800

    Added email form

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-
+const nodemailer = require("nodemailer");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -54,6 +57,53 @@ app.get("/pwaicons/manifest-icon-512.png", (request, response) => {
 app.get("*", (request, response) => {
   response.sendFile(__dirname + "/views/404.html");
 });
+
+// async..await is not allowed in global scope, must use a wrapper
+app.post("*", function(req, res) {
+
+  const smtpTransport = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user: "outlook_390775E203B45137@outlook.com",
+      pass: "Daisy1868"
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false
+    }
+  });
+
+  const mailContent = {
+    from: "outlook_390775E203B45137@outlook.com",
+    to: "outlook_390775E203B45137@outlook.com",
+    subject: "Fan Mail!!!",
+    generateTextFromHTML: true,
+    html: `<b>Name</b>: ${req.body.name}<br>
+<b>Email</b>: ${req.body.email}<br><br>
+<b>Message</b>: ${req.body.message}`
+  };
+
+  smtpTransport.sendMail(mailContent, (err, response) => {
+    //error ? console.log(error) : response.send("Your message has been sent!");
+    if (err) {
+      //response.send("Sorry, this form was not sent. Try again later?");
+      console.error(err);
+      var status = { status: "404" };
+      response.send(status);
+      console.log("Error!");
+    } else {
+      var status = { status: "200" };
+      response.send(status);
+      console.log("Sucess!");
+    }
+    smtpTransport.close();
+  });
+
+  // res.redirect("/success");
+});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
